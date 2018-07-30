d1559649718bd882a1535c4db3ab9fbb7516c2f9
xtof <christophe@openflexo.org>
2018-07-30 15:36:08 +0200

    [FIX] remove useless headers and and utf-8 encoding header

--- pouaite/models/ir_view.py
+++ pouaite/models/ir_view.py
@@ -1,4 +1,5 @@
-# Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
+# -*- coding: utf-8 -*-
+
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import fields, models
