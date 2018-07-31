c3176f8394dd7e5a0301909731431b50740181ab
mmazouff <maxime.mazouffre@imt-atlantique.net>
2018-07-31 16:23:09 +0200

    there is a beug on js

--- tableau/static/src/js/tableau.js
+++ tableau/static/src/js/tableau.js
@@ -13,9 +13,12 @@ odoo.define('tableau.tableau', function (require) {
     var _t = core._t;
     var _lt = core._lt;
     var QWeb = core.qweb;
-	
-    this.form_template = 'Tableau';
-    var rendered = QWeb.render(this.form_template, this);
-    this.$el.html(rendered);
+    
 
+    renderElement: function() {
+        this._super();
+        this.form_template = 'Tableau';
+        var rendered = QWeb.render(this.form_template, this);
+        this.$el.html(rendered);
+    }
 });
