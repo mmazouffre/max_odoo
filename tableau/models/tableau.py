fba4b74d00c7f19c0d1ffa9f94903ffda5baff7b
mmazouff <maxime.mazouffre@imt-atlantique.net>
2018-07-31 15:10:05 +0200

    added js for html teamplate

--- tableau/models/tableau.py
+++ tableau/models/tableau.py
@@ -6,6 +6,7 @@ from odoo import api, fields, models, tools, SUPERUSER_ID, _
 from odoo.exceptions import UserError, ValidationError
 from odoo.tools.safe_eval import safe_eval
 
-class Tableau(models.Model):
+class tableau(models.Model):
+
     _name = "tableau.tableau"
     _description = "Tableau"
---
+++ tableau/static/src/js/tableau.js
@@ -0,0 +1,21 @@
+
+
+odoo.define('tableau.tableau', function (require) {
+    "use strict";
+
+
+    var core = require('web.core');
+    var form_common = require('web.form_common');
+    var Model = require('web.DataModel');
+    var time = require('web.time');
+    var View = require('web.View');
+
+    var _t = core._t;
+    var _lt = core._lt;
+    var QWeb = core.qweb;
+	
+    this.form_template = 'Tableau';
+    var rendered = QWeb.render(this.form_template, this);
+    this.$el.html(rendered);
+
+}
