4daf5d0578fe449353d101af59895beeaf89f01e
mmazouff <maxime.mazouffre@imt-atlantique.net>
2018-08-21 11:13:40 +0200

    [FIX] removed useless code

--- tableau/static/src/js/tableau.js
+++ tableau/static/src/js/tableau.js
@@ -16,58 +16,7 @@ odoo.define('tableau.tableau', function (require) {
     var _lt = core._lt;
     var QWeb = core.qweb;
 
-    
-	KanbanRecord.include({
-		on_card_clicked: function () {
-		    if (this.model === 'project.task') {
-		        this.$('.o_project_kanban_boxes a').first().click();
-		    } else {
-		        this._super.apply(this, arguments);
-		    }
-		},
-		on_kanban_action_clicked: function (ev) {
-		    var self = this;
-		    if (this.model === 'project.task' && $(ev.currentTarget).data('type') === 'set_cover') {
-		        ev.preventDefault();
-
-		        new Model('ir.attachment').query(['id', 'name'])
-		           .filter([['res_model', '=', 'project.task'], ['res_id', '=', this.id], ['mimetype', 'ilike', 'image']])
-		           .all().then(open_cover_images_dialog);
-		    } else {
-		        this._super.apply(this, arguments, ev);
-		    }
-
-		    function open_cover_images_dialog(attachment_ids) {
-		        var cover_id = self.record.displayed_image_id.raw_value[0];
-		        var $content = $(QWeb.render("project.SetCoverModal", {
-		            cover_id: cover_id,
-		            attachment_ids: attachment_ids,
-		        }));
-		        var $imgs = $content.find('img');
-
-		        var dialog = new Dialog(self, {
-		            title: _t("Set a Cover Image"),
-		            buttons: [{text: _t("Select"), classes: 'btn-primary', close: true, disabled: !cover_id, click: function () {
-		                self.update_record({data: {displayed_image_id: $imgs.filter('.o_selected').data('id')}});
-		            }}, {text: _t("Remove Cover Image"), close: true, click: function () {
-		                self.update_record({data: {displayed_image_id: 0}});
-		            }}, {text: _t("Discard"), close: true}],
-		            $content: $content,
-		        }).open();
-
-		        var $selectBtn = dialog.$footer.find('.btn-primary');
-		        $content.on('click', 'img', function (ev) {
-		            $imgs.not(ev.currentTarget).removeClass('o_selected');
-		            $selectBtn.prop('disabled', !$(ev.currentTarget).toggleClass('o_selected').hasClass('o_selected'));
-		        });
-
-		        $content.on('dblclick', 'img', function (ev) {
-		            self.update_record({data: {displayed_image_id: $(ev.currentTarget).data('id')}});
-		            dialog.close();
-		        });
-		    }
-		},
-	});    
+       
 
     var myView = View.extend({
 	template: "Dash",
